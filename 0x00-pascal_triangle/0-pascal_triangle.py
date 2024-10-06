defd324d92871d758ed47facfae2aa350e56965e
bayoalliy <adebayoalliy@gmail.com>
2024-10-06 16:13:14 +0000

    pascal triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+"""
+function that creates a pascal triangle
+"""
+
+def pascal_triangle(n):
+    triangle = []
+
+    for row in range(n):
+        if row == 0:
+            triangle.append([1])
+        else:
+            last_row = triangle[-1]
+
+            idx = 0
+            new_row = []
+
+            for num in last_row:
+                if idx == 0:
+                    new_row.append(1)
+
+                else:
+                    new_row.append(num + last_row[idx - 1])
+                
+                if idx == len(last_row) - 1:
+                    new_row.append(1)
+                idx += 1
+            triangle.append(new_row)
+    return (triangle)
