306d12c6f86e78dcb66980d43c017b162f5671d7
bayoalliy <adebayoalliy@gmail.com>
2024-11-06 16:28:35 +0000

    utf

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -4,21 +4,40 @@ determines if a given data set represents a valid UTF-8 encoding."""
 
 
 def validUTF8(data):
-    """utf-8 validating function"""
-    for ch in data:
-        i = 0
-        tmp_ch = ch
-        while ch > 0:
-            ch >>= 1
-            i += 1
-        ch = tmp_ch
-        if ch < 128:
-            continue
-        elif ch < 2048 and i != 16:
+    """utf validating function"""
+    metadata = {
+                    '0': 0,
+                    '110': 1,
+                    '1110': 2,
+                    '11110': 3
+                }
+    sbsqnt_byte = 0
+
+    for char in data:
+        bin = []
+        header = ""
+
+        for i in range(8):
+            bin.append(char & 1)
+            char >>= 1
+        bin.reverse()
+
+        for i in range(5):
+            header += str(bin[i])
+            if bin[i] == 0:
+                break
+
+        if header[-1] != '0':
             return False
-        elif ch < 65536 and i != 24:
-            return False
-        elif ch > 65535 and i != 32:
-            return False
+
+        if sbsqnt_byte == 0:
+            if header not in metadata.keys():
+                return False
+            sbsqnt_byte = metadata[header]
+
+        elif sbsqnt_byte:
+            if header != '10':
+                return False
+            sbsqnt_byte -= 1
 
     return True
