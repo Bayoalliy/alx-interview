138cf5e6e2d1f3970f595498ee594126be725498
bayoalliy <adebayoalliy@gmail.com>
2024-11-13 22:22:23 +0000

    mm

---
+++ 0x05-nqueens/0-nqueens.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+""" A program that solves the N queens problem """
+import sys
+
+
+def is_safe(queens, col):
+    """checks if a queen is safe"""
+    for i in range(col):
+        if queens[i][1] == queens[col][1] or \
+            [i, queens[col][1] + (col - i)] == queens[i] or \
+                [i, queens[col][1] - (col - i)] == queens[i]:
+            return False
+    return True
+
+
+def find_nqueen(queens, col, num):
+    """recursivr function"""
+    if col >= num:
+        print(queens)
+        return
+
+    for i in range(num):
+        queens[col][1] = i
+        if is_safe(queens, col):
+            find_nqueen(queens, col + 1, num)
+    queens[col][1] = -1
+
+
+def main():
+    """main function"""
+    if len(sys.argv) is not 2:
+        print("Usage: nqueens N")
+        return(1)
+
+    try:
+        n = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        return(1)
+
+    if n < 4:
+        print("N must be at least 4")
+        return(1)
+
+    queens = [[i, -1] for i in range(n)]
+
+    find_nqueen(queens, 0, n)
+
+
+if __name__ == "__main__":
+    main()
