4057287e9f2c12cd5a681a8990261678116d5f79
bayoalliy <adebayoalliy@gmail.com>
2024-12-01 09:01:01 +0000

    lockebox

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -2,17 +2,12 @@
 """Solving lockboxes problem"""
 
 
-def unlock(boxes, idx, keys):
-    """recursively unlocks the boxes"""
-    keys.append(idx)
-    for i in boxes[idx]:
-        if i >= 0 and i < len(boxes) and i not in keys:
-            unlock(boxes, i, keys)
-    return
-
-
 def canUnlockAll(boxes):
     """Returns true if every box is unlocked"""
     keys = []
-    unlock(boxes, 0, keys)
-    return (len(keys) == len(boxes))
+    unlocked = [0]
+    for i in unlocked:
+        for j in boxes[i]:
+            if j not in unlocked and j < len(boxes):
+                unlocked.append(j)
+    return(len(unlocked) == len(boxes))
