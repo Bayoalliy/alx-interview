c043dc68cb626c2dda325e36f644f0714bf4b149
bayoalliy <adebayoalliy@gmail.com>
2024-12-03 17:14:07 +0000

    island

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -10,14 +10,13 @@ def island_perimeter(grid):
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             if grid[i][j] == 1:
-                perimeter += 4
-                if grid[i - 1][j] == 1:
-                    perimeter -= 1
-                if grid[i + 1][j] == 1:
-                    perimeter -= 1
-                if grid[i][j - 1] == 1:
-                    perimeter -= 1
-                if grid[i][j + 1] == 1:
-                    perimeter -= 1
+                if grid[i - 1][j] == 0:
+                    perimeter += 1
+                if grid[i + 1][j] == 0:
+                    perimeter += 1
+                if grid[i][j - 1] == 0:
+                    perimeter += 1
+                if grid[i][j + 1] == 0:
+                    perimeter += 1
 
     return perimeter
