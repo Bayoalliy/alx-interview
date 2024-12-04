170d0b02f75a507afe8859e29707c73ae8aab6bf
bayoalliy <adebayoalliy@gmail.com>
2024-12-04 23:26:33 +0000

    gg

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -6,6 +6,9 @@ calculating the perimeter of the island described in grid
 
 def island_perimeter(grid):
     """returns the perimeter of island"""
+    if type(grid) != list:
+        return 0
+
     perimeter = 0
     for i in range(len(grid)):
         for j in range(len(grid[i])):
